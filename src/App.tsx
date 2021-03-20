bab4615c747de96296e8469f41d898c7b1a8f356
allexis <allexissoares@gmail.com>
2021-03-20 11:49:39 -0300

    :sparkles: add some components with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { GlobalStyle } from './styles/global';
+
+import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
+  );
 }
