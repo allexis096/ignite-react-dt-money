a71aa313ec1bebf68799483857a118be1f1182ad
allexis <allexissoares@gmail.com>
2021-03-20 12:01:45 -0300

    :sparkles: add Transaction component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import * as S from './styles';
 import { Summary } from '../Summary';
+import { TransactionsTable } from '../TransactionsTable';
 
 export function Dashboard() {
   return (
     <S.Container>
       <Summary />
+      <TransactionsTable />
     </S.Container>
   );
 }
